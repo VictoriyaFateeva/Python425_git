3ff039569372ecb015df96af6702e09ad735e39d
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2024-10-12 15:19:42 +0300

    Задан ввод данных пользователя.

--- regex.py
+++ regex.py
@@ -10,3 +10,21 @@ if bool(reg_name.match(user_name)) is True:
     print("Имя принято!")
 else:
     print("Ввод некорректен! Повторите ввод данных!\n")
+
+user_last_name = input("Введите фамилию пользователя: ")
+if bool(reg_surname.match(user_last_name)) is True:
+    print("Фамилия принята!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
+
+user_phone = input("Введите телефон пользователя: ")
+if bool(reg_phone.match(user_phone)) is True:
+    print("Телефон принят!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
+
+user_mail = input("Введите email пользователя на яндексе: ")
+if bool(reg_email.match(user_mail)) is True:
+    print("email принят!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
