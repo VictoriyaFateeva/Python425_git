46626a736bfc7a29751ed44ccbcdbc98f4c9207c
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2024-10-12 15:06:50 +0300

    Задан ввод имени пользователя.

--- regex.py
+++ regex.py
@@ -5,5 +5,8 @@ reg_surname = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
 reg_phone = re.compile(r'^[+]\d{1,3}\(\d{1,3}\)(\d{7})$')
 reg_email = re.compile(r'^[A-Za-z0-9_]+@yandex\.ru$')
 
-
-
+user_name = input("Введите имя пользователя: ")
+if bool(reg_name.match(user_name)) is True:
+    print("Имя принято!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
